97c7fe6c505fa73ca74858911bd3b508bfbb2ad7
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-05-20 19:15:01 +0500

    Update Sidebar

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -5,33 +5,43 @@ import { Sidebar } from "../../Redux-Store/actions";
 const SideBar = ({ children, Menus }) => {
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch()
+  const [selectedMenu, setSelectedMenu] = useState(Menus[0]);
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+  };
   return (
     <div className="flex">
       <div
-        className={` ${open ? "w-52" : "w-16 "
+        className={` ${open ? "w-44" : "w-16 "
           } bg-blue-800 p-3 left-0 top-0 min-h-screen pt-8 relative duration-300`}
       >
         <div className="fixed" >
-          <div className={`duration-300 absolute cursor-pointer ${open ? '-right-10' : 'right-1'} top-0 w-7 text-white
+          <div className={`duration-300 absolute cursor-pointer ${open ? '-right-5' : 'right-1'} top-0 w-7 text-white
             z-20 ${!open}`}
             onClick={() => {
               dispatch(Sidebar())
               setOpen(!open)
             }}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20" strokeWidth={1.9} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-            </svg>
+            {open ?
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+              :
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+              </svg>
+            }
 
           </div>
           <ul className="pt-11">
             {Menus.map((Menu, index) => (
-              <NavLink to={Menu.path} key={index}  >
+              <NavLink to={Menu.path} key={index} onClick={() => handleMenuClick(Menu)}>
                 <li
-                  className={`flex  rounded-md p-2 cursor-pointer  hover:bg-light-white text-white text-sm items-center gap-x-4 
-              ${Menu.gap ? "mt-9" : "mt-2"}`}
+                  className={`flex rounded-md p-2 cursor-pointer hover:bg-light-white text-white text-sm items-center gap-x-4 
+                  ${Menu.gap ? 'mt-9' : 'mt-2'} ${Menu === selectedMenu ? 'bg-light-white' : ''}`}
                 >
                   <p className="text-white text-lg">{Menu.icon}</p>
-                  <span className={`${!open && "hidden"} origin-left duration-200`}>
+                  <span className={`${!open && "hidden"} origin-left duration-300`} >
                     {Menu.title}
                   </span>
                 </li>
--- src/data/Menu.jsx
+++ src/data/Menu.jsx
@@ -1,7 +1,7 @@
 import { BuildingStorefrontIcon, ChartBarIcon, ClipboardDocumentIcon, HomeIcon } from "@heroicons/react/24/solid";
 
 export const adminMenu = [
-  { title: "Dashboard", icon: <HomeIcon className="w-6 h-6" />, path: '/Admin' },
+  { title: "Dashboard", icon: <HomeIcon className="w-6 h-6" />, path: '/Admin'},
   { title: "Order Details", icon: <ClipboardDocumentIcon className="w-6 h-6" />, path: '/Admin/Order_Details' },
   { title: "Sale Details", icon: <ChartBarIcon className="w-6 h-6" />, path: '/Admin/Sale_Details' },
   { title: "Inventory", icon: <BuildingStorefrontIcon className="w-6 h-6" />, path: '/Admin/Inventory' },
