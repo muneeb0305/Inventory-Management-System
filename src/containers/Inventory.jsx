324528c1116ba56b68c530aad15aafc3a19bcc4c
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-05-20 17:58:24 +0500

    Make Searchbar Resusable

--- src/containers/Inventory.jsx
+++ src/containers/Inventory.jsx
@@ -6,8 +6,14 @@ import Swal from "sweetalert2";
 import { useDispatch, useSelector } from "react-redux";
 import { changeName } from "../Redux-Store/actions";
 import Button from "../components/Button/Button";
+import Searchbar from "../components/SearchBar/Searchbar";
 
 export default function Inventory() {
+    const [searchValue1, setSearchValue1] = useState('');
+    const handleSearch1 = (event) => {
+        setSearchValue1(event.target.value);
+      }
+     
     const dispatch = useDispatch()
     dispatch(changeName({ name: 'Inventory' }))
     const Toast = Swal.mixin({
@@ -31,15 +37,24 @@ export default function Inventory() {
             })
         // eslint-disable-next-line
     }, [isDelete])
+    let tableData = Data.filter(row => {
+        if (row.itemName.toLowerCase().includes(searchValue1.toLowerCase())) {
+          return true;
+        }
+        return false;
+      });
     return (
         <section>
             <div className="bg-gray-50 min-h-screen pt-20">
                 <div className="container mx-auto px-5 pt-5">
                     <div className='bg-white rounded-lg border-2 shadow-lg p-3'>
-                        <Link to="Add_Item">
-                            <Button label={'Add Item'} />
-                        </Link>
-                        <Table tableHeader={Headers} tableData={Data} />
+                        <div className="px-4 flex justify-between align-middle items-center">
+                            <Link to="Add_Item">
+                                <Button label={'Add Item'} />
+                            </Link>
+                            <Searchbar placeholder="Search By Name" value={searchValue1} onChange={handleSearch1} />
+                        </div>
+                        <Table tableHeader={Headers} tableData={tableData} />
                     </div>
                 </div>
             </div>
