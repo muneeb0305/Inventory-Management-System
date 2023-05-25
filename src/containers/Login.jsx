f49ccc789c71db0e13db9e2c235116c5c5e85424
Muneeb Ahmed <muneebahmed0305@gmail.com>
2023-05-25 13:03:59 +0500

    update dark theme in Login container

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { login } from '../features/Auth/AuthSlice';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -12,6 +12,8 @@ export default function LoginPage() {
     const dispatch = useDispatch()
     //User Array for Type dropbox
     const User = ['Admin', 'Customer']
+    //Redux States
+    const toggle = useSelector(state => state.appState.darkMode)
     const [Form, setForm] = useState({
         email: '',
         password: '',
@@ -22,7 +24,7 @@ export default function LoginPage() {
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        //check responce od api and navigate to there role
+        //check responce of api and navigate to there role
         dispatch(login({ ...Form }))
             .unwrap()
             .then((payload) => {
@@ -40,14 +42,14 @@ export default function LoginPage() {
     };
     return (
         <section>
-            <div className="bg-blue-100 h-screen w-full flex justify-center items-center">
-                <div className="bg-blue-600 w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded">
+            <div className={`${toggle?'bg-dark3':'bg-gray-100'} h-screen w-full flex justify-center items-center`}>
+                <div className={`${toggle?'bg-dark2':'bg-blue-600'} w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded`}>
                     <div className="w-full md:w-1/2 hidden md:flex flex-col justify-center items-center text-white">
                         <h1 className="text-3xl">Hello</h1>
                         <p className="text-5xl font-extrabold">Welcome!</p>
                     </div>
-                    <div className="bg-white w-full md:w-1/2 flex flex-col items-center py-32 px-8">
-                        <h3 className="text-3xl font-bold text-blue-600 mb-4">
+                    <div className={`${toggle?'bg-dark7':'bg-white'} w-full md:w-1/2 flex flex-col items-center py-32 px-8`}>
+                        <h3 className={`${toggle?'text-dark2':'text-blue-600'} text-3xl font-bold  mb-4`}>
                             LOGIN
                         </h3>
                         <form className="w-full flex flex-col justify-center" onSubmit={handleSubmit} autoComplete='off'>
@@ -56,7 +58,7 @@ export default function LoginPage() {
                             <Select label={'Type'} data={User} name='type' value={Form.type} onChange={handleChange} />
                             <Button type="submit" label={'Submit'} />
                         </form>
-                        <div className='mt-5 shadow-md px-3 py-1 border-2 rounded-lg text-gray-500 hover:text-white hover:cursor-pointer bg-white hover:bg-blue-600'>
+                        <div className='mt-5 text-gray-500 hover:text-dark2 hover:cursor-pointer '>
                             <Link to='/registration'><p className='text-sm '>Register Your Self</p></Link>
                         </div>
                     </div>
